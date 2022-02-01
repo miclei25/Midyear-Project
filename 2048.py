a2d37ca4f35f249dcfd44f342baa4eeceabf8acf
Dayanna Muriel <iiiday.ashley@gmail.com>
2022-02-01 08:32:27 -0500

    lists

--- 2048.py
+++ 2048.py
@@ -1,3 +1,9 @@
-from tkinter import *
-import random
+col1 = []
+col2 = []
+col3 = []
+col4 = []
 
+row1 = [col1, col2, col3, col4]
+row2 = [col1, col2, col3, col4]
+row3 = [col1, col2, col3, col4]
+row4 = [col1, col2, col3, col4]
