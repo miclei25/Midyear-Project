05447974a36a5dc8320238168153a42c5d9d71e5
Ella Marmol <embmarmol@gmail.com>
2022-02-02 08:47:29 -0500

    added play button method

--- 2048openingGUI.py
+++ 2048openingGUI.py
@@ -1,21 +1,25 @@
 from tkinter import *
 
 class Screen_Opening(Frame):
-    def __init__(self, master):
+    def __init__(self, master, callback_on_play):
         super().__init__(master)
+
+        self.callback_on_play = callback_on_play
 
         self.create_widgets()
         self.grid()
 
     def create_widgets(self):
 
-        Label(self, text = "2048", font = "Georgia 45 bold", fg = "Hot Pink").grid(row = 0, column = 1)
+        Label(self, text = "").grid(row = 0)
         Label(self, text = "").grid(row = 1)
-        Label(self, text = "How to Play:\n\nUse buttons to move around the boxes within the grid.\nBoxes with the same number merge into one when they touch.\nTry to reach 2048.", font = "Helvetica 15 italic", fg = "Pink").grid(row = 2, column = 0, columnspan = 3)
+        Label(self, text = "").grid(row = 2)
+        Label(self, text = "2048", font = "Georgia 45 bold", fg = "Hot Pink").grid(row = 3, column = 1)
+        Label(self, text = "").grid(row = 4)
+        Label(self, text = "How to Play:\n\nUse buttons to move around the boxes within the grid.\nBoxes with the same number merge into one when they touch.\nTry to reach 2048.", font = "Helvetica 15 italic", fg = "VioletRed3").grid(row = 5, column = 0, columnspan = 3)
+        Label(self, text = "").grid(row = 6)
+        Button(self, text = "Press to Play", font = "Courier 12 bold", fg = "Maroon3", command = self.selected_play).grid(row = 7, column = 1)
 
+    def selected_play(self):
+        self.callback_on_play()
 
-root = Tk()
-root.title("2048")
-root.geometry("555x500")
-app = Screen_Opening(root)
-root.mainloop()
