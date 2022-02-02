4b8a2f2645ba84fdeb40633d896b35502f3ae6d0
anaajesh <anaghaajesh2010@gmail.com>
2022-02-02 08:58:26 -0500

    pulled

--- 2048manager.py
+++ 2048manager.py
@@ -4,7 +4,6 @@ from OpeningGUI import Screen_Opening
 class GameManager(object):
 
     def __init__(self):
-<<<<<<< HEAD
         self.root = Tk()
     
     def setup_openingscreen(self):
@@ -20,18 +19,3 @@ class GameManager(object):
         self.root.title ("2048")
         self.root.geometry ("555x500")
         
-=======
-        self.root = tkinter.Tk()
-
-    def opening_screen_stuff(self):
-
-        self.root.title = "Welcome to 2048!"
-
-    def game_screen(self):
-
-        self.root.title = "2048"
-
-def main():
-    pass
-    
->>>>>>> 9c4666f46bf5fe6dbd3ce2568200c8fd38692a1a
