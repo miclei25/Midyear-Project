433d5ee6b068a211903cc3d85f567c7da3fec939
anaajesh <anaghaajesh2010@gmail.com>
2022-02-02 08:48:20 -0500

    methodsforscreens

--- 2048manager.py
+++ 2048manager.py
@@ -6,7 +6,14 @@ class GameManager(object):
     def __init__(self):
         self.root = tkinter.Tk()
 
-  
+    def opening_screen_stuff(self):
+
+        self.root.title = "Welcome to 2048!"
+
+    def game_screen(self):
+
+        self.root.title = "2048"
+
 def main():
     pass
     
