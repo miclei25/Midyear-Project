1ead31bde906990a6ac90b2a07a430f462efe262
niclei25 <nikaleifer@gmail.com>
2022-02-03 08:05:49 -0500

    minor changes

--- OpeningGUI.py
+++ OpeningGUI.py
@@ -16,7 +16,9 @@ class Screen_Opening(Frame):
         Label(self, text = "").grid(row = 2)
         Label(self, text = "2048", font = "Georgia 45 bold", fg = "Hot Pink").grid(row = 3, column = 1)
         Label(self, text = "").grid(row = 4)
-        Label(self, text = "How to Play:\n\nUse buttons to move around the boxes within the grid.\nBoxes with the same number merge into one when they touch.\nTry to reach 2048.", font = "Helvetica 15 italic", fg = "VioletRed3").grid(row = 5, column = 0, columnspan = 3)
+        Label(self, text = "How to Play:\n\nUse buttons to move around the boxes within the grid.\nBoxes with the same number merge into one when they touch.\nTry to reach 2048.", 
+                    font = "Helvetica 15 italic", fg = "VioletRed3"
+                    ).grid(row = 5, column = 0, columnspan = 3)
         Label(self, text = "").grid(row = 6)
         Button(self, text = "Press to Play", font = "Courier 12 bold", fg = "Maroon3", command = self.selected_play).grid(row = 7, column = 1)
 
