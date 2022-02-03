e5cb656d73ff1aa87fcec0046bea8325141e5f8a
Ella Marmol <embmarmol@gmail.com>
2022-02-03 08:44:44 -0500

    gameGUI init

--- gameGUI.py
+++ gameGUI.py
@@ -2,9 +2,15 @@ from tkinter import *
 
 class Game_Screen(Frame):
 
-    def __init__(self):
-        pass
+    def __init__(self, master, callback_on_exit):
+        super().__init__(master)
+
+        self.callback_on_exit = callback_on_exit
+
+        self.create_widgets()
+        self.grid()
 
     def create_widgets(self):
-        Label(self, text = "2048", font = "Georgia 45 bold", fg = "Hot Pink").grid(row = 3, column = 1)
-        Label(self, text = "").grid(row = 4)
+        Label(self, text = "2048", font = "Georgia 25 bold", fg = "Hot Pink").grid(row = 0, column = 1)
+        Label(self, text = "").grid(row = 1)
+        
