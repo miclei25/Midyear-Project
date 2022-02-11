da9ca6b3382b296e6b2cd7143e66b1756d428309
niclei25 <nikaleifer@gmail.com>
2022-02-11 16:37:27 -0500

    some edits - functions don't work still

--- Model.py
+++ Model.py
@@ -51,21 +51,21 @@ class Grid():
                     else:
                         prev_cell = cell
             
-            prev_cell = None
+            empty_cell = None
             for cell in range(len(self.grid[r])):
-                if prev_cell == None:
-                    prev_cell = cell
+                for c in range(len(self.grid[r])):
+                    if self.grid[r][c] == None:
+                        empty_cell = c
+                        break
                 
-                elif self.grid[r][cell] == None and self.grid[r][prev_cell] != None:
-                    for c in range(len(self.grid[r])):
-                        if self.grid[r][c] == None:
-                            prev_cell = c
-                            break
-
-                elif self.grid[r][cell] != None and self.grid[r][prev_cell] == None:
-                    self.grid[r][prev_cell] = self.grid[r][cell]
-                    self.grid[r][cell] = None
-                    move = True
+                if empty_cell != None:
+                    for c in range(len(self.grid[r][empty_cell + 1:])):
+                        if self.grid[r][empty_cell + 1 + c] != None:
+                            self.grid[r][empty_cell] = self.grid[r][empty_cell + 1 + c]
+                            self.grid[r][empty_cell + 1 + c] = None
+                            move = True
+                else:
+                    break
         
         if move == True:
             self.add_number()
@@ -91,21 +91,21 @@ class Grid():
                     else:
                         prev_cell = 3 - c
             
-            prev_cell = None
-            for c in range(len(self.grid[r])):
-                if prev_cell == None:
-                    prev_cell = 3 - c
+            empty_cell = None
+            for cell in range(len(self.grid[r])):
+                for c in range(len(self.grid[r])):
+                    if self.grid[r][3 - c] == None:
+                        empty_cell = 3 - c
+                        break
                 
-                elif self.grid[r][3 - c] == None and self.grid[r][prev_cell] != None:
-                    for c in range(len(self.grid[r])):
-                        if self.grid[r][3 - c] == None:
-                            prev_cell = 3 - c
-                            break
-
-                elif self.grid[r][3 - c] != None and self.grid[r][prev_cell] == None:
-                    self.grid[r][prev_cell] = self.grid[r][3 - c]
-                    self.grid[r][3 - c] = None
-                    move = True
+                if empty_cell != None:
+                    for c in range(len(self.grid[r][:empty_cell + 1])):
+                        if self.grid[r][c] != None:
+                            self.grid[r][empty_cell] = self.grid[r][c]
+                            self.grid[r][c] = None
+                            move = True
+                else:
+                    break
 
         if move == True:
             self.add_number()
@@ -130,21 +130,21 @@ class Grid():
                     else:
                         prev_cell = r
             
-            prev_cell = None
-            for r in range(len(self.grid[c])):
-                if prev_cell == None:
-                    prev_cell = r
+            empty_cell = None
+            for row in range(len(self.grid[c])):
+                for r in range(len(self.grid[c])):
+                    if self.grid[r][c] == None:
+                        empty_cell = r
+                        break
                 
-                elif self.grid[r][c] == None and self.grid[prev_cell][c] != None:
-                    for r in range(len(self.grid[c])):
-                        if self.grid[r][c] == None:
-                            prev_cell = r
-                            break
-
-                elif self.grid[r][c] != None and self.grid[prev_cell][c] == None:
-                    self.grid[prev_cell][c] = self.grid[r][c]
-                    self.grid[r][c] = None
-                    move = True
+                if empty_cell != None:
+                    for r in range(len(self.grid[:empty_cell + 1][c])):
+                        if self.grid[empty_cell + 4 - r][c] != None:
+                            self.grid[r][empty_cell] = self.grid[empty_cell + 4 - r][c]
+                            self.grid[empty_cell + 4 - r][c] = None
+                            move = True
+                else:
+                    break
         
         if move == True:
             self.add_number()
@@ -154,7 +154,38 @@ class Grid():
     def move_down(self):
         move = False
         
-        # code
+        for c in range(4):
+            prev_cell = None
+            for r in range(len(self.grid[c])):
+
+                if self.grid[r][c] != None:
+                    if prev_cell == None:
+                        prev_cell = r
+                    
+                    elif self.grid[r][c] == self.grid[prev_cell][c]:
+                        self.grid[r][c] *= 2
+                        self.grid[prev_cell][c] = None
+                        self.score += self.grid[r][c]
+                        move = True
+                    
+                    else:
+                        prev_cell = r
+            
+            empty_cell = None
+            for row in range(len(self.grid[c])):
+                for r in range(len(self.grid[c])):
+                    if self.grid[r][c] == None:
+                        empty_cell = r
+                        break
+                
+                if empty_cell != None:
+                    for r in range(len(self.grid[:empty_cell + 1][c])):
+                        if self.grid[empty_cell + 4 - r][c] != None:
+                            self.grid[r][empty_cell] = self.grid[empty_cell + 4 - r][c]
+                            self.grid[empty_cell + 4 - r][c] = None
+                            move = True
+                else:
+                    break
 
         if move == True:
             self.add_number()
