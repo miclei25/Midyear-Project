f628c27f21d9e9a4d40805a98d152178991ad55e
anaajesh <anaghaajesh2010@gmail.com>
2022-02-15 08:38:47 -0500

    sensical buttons

--- gameGUI.py
+++ gameGUI.py
@@ -46,14 +46,14 @@ class Game_Screen(Frame):
                 piclabel.grid(row = row + 2, column = col + 1)
                 
         Label(self, text = "").grid(row = 8)
-        Button(self, text = "UP", font = "Helvetica 10 bold", fg = "DeepPink", command = self.up
-        ).grid(row = 9, column = 0, sticky = E, columnspan = 4)
-        Button(self, text = "DOWN", font = "Helvetica 10 bold", fg = "DeepPink", command = self.down
+        Button(self, text = " UP ", font = "Helvetica 10 bold", fg = "DeepPink", command = self.up
         ).grid(row = 9, column = 2, sticky = W, columnspan = 4)
+        Button(self, text = "DOWN", font = "Helvetica 10 bold", fg = "DeepPink", command = self.down
+        ).grid(row = 9, column = 0, sticky = E, columnspan = 4)
         Button(self, text = "LEFT", font = "Helvetica 10 bold", fg = "DeepPink", command = self.left
-        ).grid(row = 10, column = 0, sticky = E, columnspan = 4)
-        Button(self, text = "RIGHT", font = "Helvetica 10 bold", fg = "DeepPink", command = self.right
         ).grid(row = 10, column = 2, sticky = W, columnspan = 4)
+        Button(self, text = "RIGHT", font = "Helvetica 10 bold", fg = "DeepPink", command = self.right
+        ).grid(row = 10, column = 0, sticky = E, columnspan = 4)
         
         Label(self, text = "\n", bg = bg_color).grid(row = 10)
         Button(self, text = "Exit", font = "Courier 12 bold", fg = "black", bg = "Hot Pink", command = self.selected_exit
