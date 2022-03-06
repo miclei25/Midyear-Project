7e4c75af0f64f9c8acbd6468ceba80163d6cf74d
anaajesh <anaghaajesh2010@gmail.com>
2022-03-06 16:44:03 -0500

    losing screen

--- gameGUI.py
+++ gameGUI.py
@@ -76,6 +76,8 @@ class Game_Screen(Frame):
         ).grid(row = 11, column = 1, columnspan = 4)
         Label(self, text = "", bg = bg_color).grid(row = 12)
 
+
+
     def display_score(self):
         self.total_score.set(str(self.grid1.score))
 
@@ -85,12 +87,8 @@ class Game_Screen(Frame):
         self.grid1.move_up = False
         self.display_grid()
         self.display_score()
+        self.losing_screen()
         # self.lose()
-
-    #def losing_screen(self):
-        #for row in range(4):
-            #for col in range(4):
-                #if self.grid1.grid[row][col] == self.grid1.grid[row+1][col] or self.grid1.grid[row][col] == self.grid1.grid[row-1][]
 
     def down(self, event = None):
         self.grid1.move_down = True
@@ -98,6 +96,7 @@ class Game_Screen(Frame):
         self.grid1.move_down = False
         self.display_grid()
         self.display_score()
+        self.losing_screen()
         # self.lose()
 
     def left(self, event = None):
@@ -106,6 +105,7 @@ class Game_Screen(Frame):
         self.grid1.move_left = False
         self.display_grid()   
         self.display_score()
+        self.losing_screen()
         # self.lose()
 
     def right(self, event = None):
@@ -114,10 +114,26 @@ class Game_Screen(Frame):
         self.grid1.move_right = False
         self.display_grid()
         self.display_score()
+        self.losing_screen()
         # self.lose()
         
     def selected_exit(self):
         self.callback_on_exit()
+
+    def losing_screen(self):
+         if self.grid1.can_move_horiz == False and self.grid1.can_move_vert == False:
+            image = PhotoImage(file="images/iwang.gif")
+            piclabel = Label(self, image = image, bg = "Hot Pink", borderwidth = "50px")
+            self.imagelabels.append(piclabel)
+            piclabel.photo = image
+            piclabel.grid(row = 3, column = 1, columnspan = 4, rowspan = 4)
+            Label(self, text = "You Lose!", bg = "Hot Pink", font = "Helvetica 24", fg = "white").grid(row = 6, column = 2, columnspan = 2)
+
+
+
+    #for row in range(4):
+        #for col in range(4):
+            #if self.grid1.grid[row][col] == self.grid1.grid[row+1][col] or self.grid1.grid[row][col] == self.grid1.grid[row-1][]
     
     # def lose(self):
     #     if self.grid1.can_move_horiz == False and self.grid1.can_move_vert == False:
